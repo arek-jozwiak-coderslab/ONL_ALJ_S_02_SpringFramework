1c060da495f4cdf71c2f912c4d9bfb12bfedd2b6
arcyro <arek@arcyro.pl>
2025-08-21 09:51:31 +0200

    web

--- src/main/java/pl/coderslab/mvc/AppConfig.java
+++ src/main/java/pl/coderslab/mvc/AppConfig.java
@@ -2,6 +2,7 @@ package pl.coderslab.mvc;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -9,4 +10,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 @ComponentScan(basePackages = "pl.coderslab.mvc")
 public class AppConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configureDefaultServletHandling(
+            DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
 }
--- src/main/java/pl/coderslab/mvc/HomeController.java
+++ src/main/java/pl/coderslab/mvc/HomeController.java
@@ -1,16 +1,31 @@
 package pl.coderslab.mvc;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequestMapping("/start")
 public class HomeController {
+
+    Logger log = LoggerFactory.getLogger(HomeController.class);
 
     @GetMapping("/")
     @ResponseBody
     public String home() {
+        int a = 5;
+        int b = 15;
+        log.info("home {} qweqw  {}", a , b);
+        return "hello world";
+    }
+
+    @GetMapping("/home")
+    @ResponseBody
+    public String home2() {
         return "hello world";
     }
 
